9367df6d8e6ffdb7460cc952bd66d521e8836947
Santiago Pardo Morales <s.pardom@uniandes.edu.co>
2023-10-01 14:28:17 -0500

    restart exam

--- parcial1/src/App.js
+++ parcial1/src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import './App.css'; // Asegúrate de ajustar la ruta a tu archivo de estilos si es necesario
-import Login from './Login'; // Importa el componente Login
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <header className="App-header">
-    <h2 className="text-sm">Acceder</h2>
-    <h3 className="text-sm">Usa tu cuenta de Unialpes</h3>
-    </header>
-      <main>
-        <Login /> {/* Renderiza el componente Login aquí */}
-      </main>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
-
 
